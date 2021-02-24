17dd15207ce914f7c4304ce7d84bdf285b87cca3
vinay-sj <vinaysra@gmail.com>
2021-02-23 19:39:53 -0500

    commented out unused code

--- src/Components/StepperContents.js
+++ src/Components/StepperContents.js
@@ -5,31 +5,31 @@ import {Col, Row} from "react-bootstrap";
 import {GetExperienceImage} from "./ImageLoader";
 import StepConnector from '@material-ui/core/StepConnector';
 
-const useColorlibStepIconStyles = makeStyles({
-    root: {
-        backgroundColor: "#ccc",
-        zIndex: 1,
-        color: "#fff",
-        width: 50,
-        height: 50,
-        display: "flex",
-        borderRadius: "50%",
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    active: {
-        backgroundImage:
-            "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
-        boxShadow: "0 4px 10px 0 rgba(0,0,0,.25)"
-    },
-    completed: {
-        backgroundImage:
-            "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)"
-    }
-});
+// const useColorlibStepIconStyles = makeStyles({
+//     root: {
+//         backgroundColor: "#ccc",
+//         zIndex: 1,
+//         color: "#fff",
+//         width: 50,
+//         height: 50,
+//         display: "flex",
+//         borderRadius: "50%",
+//         justifyContent: "center",
+//         alignItems: "center"
+//     },
+//     active: {
+//         backgroundImage:
+//             "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
+//         boxShadow: "0 4px 10px 0 rgba(0,0,0,.25)"
+//     },
+//     completed: {
+//         backgroundImage:
+//             "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)"
+//     }
+// });
 
 function StepperContents({experiences}) {
-    const classes = useColorlibStepIconStyles();
+    // const classes = useColorlibStepIconStyles();
     const experienceKeys = Object.keys(experiences);
 
     const [activeStep, setActiveStep] = useState(0);
