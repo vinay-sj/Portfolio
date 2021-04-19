1db749b1e0c147c1c3f2c181b5f5c98845b3ec1d
vinay-sj <vinaysra@gmail.com>
2021-04-19 00:53:58 -0400

    saving state to local storafe

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import './App.css';
 import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
 import Body from "./Components/Body";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {ThemeProvider} from "@material-ui/core/styles";
 import {appTheme} from "./css/Theme";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
 function App() {
-    const [lightMode, setLightMode] = useState(false);
+    const [lightMode, setLightMode] = useState(() => {
+        const mode = window.localStorage.getItem("displayModeVJ");
+        return mode !== null
+            ? JSON.parse(mode)
+            : false;
+    })
     const theme = appTheme(lightMode);
-
+    useEffect(() => {
+        window.localStorage.setItem("displayModeVJ", JSON.stringify(lightMode));
+    }, [lightMode]);
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline/>
